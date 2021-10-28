dbfaa4274e7c34e2b738ae845e8176d101954af3
Aly Neumann <aly.neumann@dakiadigital.com>
2021-10-28 14:33:41 -0400

    added secure cookies and parser

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const express = require('express');
 const app = express();
 const dayjs = require('dayjs');
 const conversionService = require('./conversion-service');
+const cookieParser = require('cookie-parser');
 require('dotenv').config();
 
 const port = process.env.PORT || 3006;
 app.use(express.json());
 app.use(express.text());
+app.use(cookieParser());
 
 app.listen(port);
 
@@ -16,14 +18,26 @@ app.get('/', async function (req, res, next){
         secret: process.env.SECRET
     }
     res.cookie("secureCookie", JSON.stringify(cookieData), {
+        secure: true,
         httpOnly: true,
         expires: dayjs().add(1, "days").toDate()
     })
-    res.send("hi")
+    res.send("hi, here is your fresh cookie")
 })
 
 app.post('/', async function (req, res, next) {
+    if(!req.cookies || req.secureCookie){
+        return "session expired"
+    }
     try {
+        const cookieData = {
+            secret: process.env.SECRET
+        }
+        res.cookie("secureCookie", JSON.stringify(cookieData), {
+            secure: true,
+            httpOnly: true,
+            expires: dayjs().add(1, "days").toDate()
+        })
         let result = await conversionService.createPdfFromUrl(req.body.url, req.body.email);
         res.send(result);
     }
